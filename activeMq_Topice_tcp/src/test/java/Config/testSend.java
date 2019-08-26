a41d11940dc857a1968c1b953a77f557d3ad2320
yanyan <yanyan@cmhi.chinamobile.com>
2019-08-26 12:31:35 +0800

    no message

--- activeMq_Topice_tcp/src/test/java/Config/testSend.java
+++ activeMq_Topice_tcp/src/test/java/Config/testSend.java
@@ -3,24 +3,55 @@ package Config;
 
 import Config.Controller.Producer;
 import bo.DeviceOnline;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
-
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+@Slf4j
 @SpringBootTest
 @RunWith(SpringRunner.class)
 public class testSend {
 
     @Autowired
     Producer producer;
+
     @Test
-    public void send()
+    public void testLocale()
     {
+        //①带有语言和国家/地区信息的本地化对象
+        Locale locale1 = new Locale("zh","CN");
+
+        //②只有语言信息的本地化对象
+        Locale locale2 = new Locale("zh");
+
+        //③等同于Locale("zh","CN")
+        Locale locale3 = Locale.CHINA;
+
+        //④等同于Locale("zh")
+        Locale locale4 = Locale.CHINESE;
+
+        //⑤获取本地系统默认的本地化对象
+        Locale locale5= Locale.getDefault();
+
+        Locale locale = new Locale("zh", "CN");
+        NumberFormat currFmt = NumberFormat.getCurrencyInstance(locale);
+        double amt = 123456.78;
+        System.out.println(currFmt.format(amt));
+
+
+
+    }
+
+
+    @Test
+    public void testSendtoBp() throws ParseException {
         DeviceOnline deviceOnline = new DeviceOnline();
         deviceOnline.setCmdType("REPORT_LAN_DEVICE_ONLINE");
         deviceOnline.setSequenceId("FFFFFFFF");
@@ -29,13 +60,13 @@ public class testSend {
         deviceOnline.setRpcMethod("Get");
 
         DeviceOnline.Parameter parameter = new DeviceOnline.Parameter();
-        parameter.setDeviceMac("333");
-        parameter.setMac("234");
+        parameter.setDeviceMac("3233");
+        parameter.setMac("2384");
         parameter.setDeviceName("abc");
         parameter.setDhcpName("aaa");
 
-//        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-//        parameter.setUpTime(sdf.parse("2014-07-26-13:23:55"));
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd-HH:mm:ss");
+        parameter.setUpTime(sdf.parse("2014-07-26-13:23:55"));
         parameter.setIp("192.168.1.1");
         parameter.setConnectType("link");
         parameter.setPort("8080");
@@ -47,8 +78,6 @@ public class testSend {
         deviceOnline.setUserId("9669951750223");
         producer.sendDeviceOnlineMessage(deviceOnline);
 //        producer.sendTextMessage("aaaa");
-
-
 
 
     }
